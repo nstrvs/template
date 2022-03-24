2ee13294278c53cb7918084a8f4e3469bcaf6f33
nstrvs <hi@nestorvalles.com>
2022-03-23 18:38:01 -0700

    Bolierplate setup

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# template
-Bolierplate for modern web development
+# Overview
+Bolierplate for modern web development using JavaScript and Webpack.
+
+# Getting started
+
+You'll need to have Node.js and NPM installed in your machine, these are the dependencies to use the repository locally.
+
+```
+# Webpack
+
+npm install --save-dev webpack webpack-cli webpack-dev-server webpack-merge clean-webpack-plugin copy-webpack-plugin mini-css-extract-plugin image-minimizer-webpack-plugin imagemin imagemin-gifsicle imagemin-jpegtran imagemin-optipng imagemin-svgo
+
+# Linters
+
+npm install --save-dev eslint stylint
+
+# Loaders
+
+npm install --save-dev css-loader postcss-loader babel-loader sass sass-loeader pug pug-loader file-loader raw-loader glslify-loader
+
+#Libraries
+
+npm install --save-dev gsap ogl
+
+```
---
+++ webpack.config.js
@@ -0,0 +1,143 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const { dirname } = require('path')
+
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
+
+const dirApp = path.join(__dirname, 'app')
+const dirShared = path.join(__dirname, 'shared')
+const dirStyles = path.join(__dirname, 'styles')
+const dirNode = 'node_modules'
+
+
+module.exports = {
+  entry: [
+    path.join(dirApp, 'index.js'),
+    path.join(dirStyles, 'index.scss')
+  ],
+
+  resolve: {
+    modules: [
+      dirApp,
+      dirShared,
+      dirStyles,
+      dirNode
+    ]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEVELOPMENT
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './shared',
+          to: ''
+        }
+      ]
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: ['pug-loader']
+      },
+
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: IS_DEVELOPMENT
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: IS_DEVELOPMENT
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: IS_DEVELOPMENT
+            }
+          }
+        ]
+      },
+
+      {
+        test: /\.(jpe?g|png|gif|svg|fnt|webp)$/,
+        type: 'asset',
+        loader: 'file-loader',
+        options: {
+          name (file) {
+            return '[hash].[ext]'
+          }
+        }
+      },
+
+      {
+        test: /\.(woff2?)$/,
+        loader: 'file-loader',
+        options: {
+          name (file) {
+            return '[name].[ext]'
+          }
+        }
+      },
+
+      {
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'raw-loader',
+        exclude: /node_modules/
+      },
+
+      {
+        test: /\.(glsl|frag|vert)$/,
+        loader: 'glslify-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  optimization: {
+    minimizer: [
+      "...",
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ["gifsicle", { interlaced: true }],
+              ["jpegtran", { progressive: true }],
+              ["optipng", { optimizationLevel: 5 }],
+            ],
+          },
+        },
+      }),
+    ],
+  },
+}
